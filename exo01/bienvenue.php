3a04c1193d932537ba540336056adcd533bd5d19
pyko <pyko@xilim.com>
2020-03-22 23:21:45 +0100

    Exos 1 et 2

---
+++ exo01/bienvenue.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Exercice 1</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+		
+		Bienvenue à vous <strong><?php echo $_GET["nom"] ; ?></strong> !
+		
+		
+	</body>
+</html>
---
+++ exo02/bienvenue.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Exercice 2</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+		
+		Bienvenue à vous <strong><?php echo $_POST["prenom"] . " " . $_POST["nom"] ; ?></strong> !
+		
+		
+	</body>
+</html>
